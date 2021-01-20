853e31f59332333bbe65c0bbb30aba6da3402445
Jessica Natovich <jessicanatovich@yahoo.com>
2021-01-20 23:59:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,2 @@
 
-  def initialize(name)
-    @name = name
-    @@all << self
-    save
-  end
-
-  def self.all
-@@ -20,4 +20,8 @@ def self.print_all
-  def self.clear_all
-  end 
-    @@all.clear
-  end
-
-  def save
-    @@all << self
-  end
-end
+ 
